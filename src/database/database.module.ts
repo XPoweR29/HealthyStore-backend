51d649f27cdc5cb8bb92c0d4d19e3f0ecba5d15e
XPoweR29 <xpower-29@wp.pl>
2023-05-13 17:33:57 +0200

    Configuration DB connection with .env variables

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'healthy_store',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       bigNumberStrings: false,
       entities: [],
       autoLoadEntities: true,
--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'healhy_store',
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT),
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   entities: [],
   migrations: [],
 });
