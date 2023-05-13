c6a4123e59cd82351a95a2d525a4e8048a676666
XPoweR29 <xpower-29@wp.pl>
2023-05-13 17:45:11 +0200

    Created shop resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ShopModule],
   controllers: [AppController],
   providers: [AppService],
 })
