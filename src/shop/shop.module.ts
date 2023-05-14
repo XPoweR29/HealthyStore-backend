2ff66e512eb0066dcb05fa17b7b0160efab47d6a
XPoweR29 <xpower-29@wp.pl>
2023-05-14 09:41:27 +0200

    Created ShopItemEntity and added it into shopModule

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShopService } from './shop.service';
 import { ShopController } from './shop.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShopItemEntity } from './entities/shop-item.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ShopItemEntity])
+  ],
   controllers: [ShopController],
   providers: [ShopService]
 })
